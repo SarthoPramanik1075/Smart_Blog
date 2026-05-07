8c0c596cef2485c19737409b247f3017652ff7c4
Sartho Pramanik <pramaniksartho1@gmail.com>
2026-05-08 03:44:07 +0600

    admin modify

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'author',
+        'created_at'
+    ]
+    search_fields = ['title']
+    list_filter = ['author', 'created_at']
+    ordering = ['-created_at'] 
